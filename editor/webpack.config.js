20eb5998b6f3d84d471c8aef1679780299dd8775
achimcc <achim.schneider@posteo.de>
2021-05-27 18:07:03 +0200

    webpack seems to work

---
+++ editor/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.tsx",
+  },
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Rust Analyzer",
+      template: "./src/index.html",
+    }),
+  ],
+};

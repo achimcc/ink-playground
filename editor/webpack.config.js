d1de959bef2759649289cc85a62caad38cb9c68f
achimcc <achim.schneider@posteo.de>
2021-05-27 18:31:06 +0200

    added monaco editor core & style loader for css

--- editor/webpack.config.js
+++ editor/webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /.tsx?$/,
         use: "ts-loader",
         exclude: ["/node_modules/"],
